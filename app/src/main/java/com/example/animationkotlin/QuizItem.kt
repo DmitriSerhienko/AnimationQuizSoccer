d5c46ab052f2309add5b9b52bd96dadfd8d27b83
Dimas <dvserhienko.pb@gmail.com>
2022-11-20 21:48:04 +0200

    Initial commit

---
+++ app/src/main/java/com/example/animationkotlin/QuizItem.kt
@@ -0,0 +1,12 @@
+package com.example.animationkotlin
+
+class QuizItem {
+    var question: String
+    var answer: List<String>
+
+    constructor(question: String, answer: List<String>) {
+        this.question = question
+        this.answer = answer
+    }
+
+}
---
+++ app/src/main/java/com/example/animationkotlin/WelcomeScreenFrag.kt
@@ -0,0 +1,31 @@
+package com.example.animationkotlin
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import com.example.animationkotlin.databinding.FragmentWelcomeScreenBinding
+
+class WelcomeScreenFrag : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val binding: FragmentWelcomeScreenBinding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_welcome_screen, container, false)
+
+
+        binding.button.setOnClickListener {view: View ->
+           Navigation.findNavController(view).navigate(R.id.action_welcomeScreenFrag2_to_quizFragment)
+        }
+        return binding.root
+
+    }
+
+
+}
